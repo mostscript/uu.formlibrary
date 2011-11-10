691f0d80c3a9280e5cb02d6d508ed186534a1472
Sean Upton <sdupton@gmail.com>
2011-11-10 14:34:59 -0700

    added utils.py, with a function to generate a dynamic schema containing a single "grid" field.  The purpose of such a schema would be as a wrapper for using data grid field.

---
+++ uu/formlibrary/utils.py
@@ -0,0 +1,31 @@
+
+from collective.z3cform.datagridfield import DictRow, DataGridFieldFactory
+from zope.schema import List
+
+from uu.dynamicschema.schema import new_schema
+
+
+WIDGET = 'collective.z3cform.datagridfield.datagridfield.DataGridFieldFactory'
+
+def grid_wrapper_schema(schema, title=u'', description=u''):
+    """
+    Given a schema interface for use in a data-grid, construct and 
+    return a wrapper form interface to use that grid (DictRow).
+    """
+    # create empty new dynamic schema
+    wrapper = new_schema()
+    # inject a field into that schema interface
+    wrapper._InterfaceClass__attrs['data'] = List(
+        title=unicode(title),
+        description=unicode(description),
+        value_type=DictRow(schema=schema),
+        )
+    # specify plone.autoform widget config for field:
+    tv = wrapper._Element__tagged_values
+    tv['__form_directive_values__'] = {
+        'plone.autoform.widgets' : {
+            'data' : WIDGET,
+            },
+        }
+    return wrapper
+

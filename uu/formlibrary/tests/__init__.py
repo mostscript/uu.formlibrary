025735770232bf43aae87028c114daf821e6dd51
Sean Upton <sdupton@gmail.com>
2011-11-14 11:49:59 -0700

    added test_request() method to uu.formlibrary.tests for use by test cases in creating a convincing fake test request that works with forms correctly.

--- uu/formlibrary/tests/__init__.py
+++ uu/formlibrary/tests/__init__.py
@@ -1,4 +1,11 @@
+import sys
 import unittest2 as unittest
+
+from zope.interface import alsoProvides
+from zope.publisher.browser import setDefaultSkin
+from z3c.form.interfaces import IFormLayer
+from ZPublisher.HTTPResponse import HTTPResponse
+from ZPublisher.HTTPRequest import HTTPRequest
 
 
 class PkgTest(unittest.TestCase):
@@ -8,4 +15,19 @@ class PkgTest(unittest.TestCase):
         """test package import, looks like zcml-initialized zope2 product"""
         import uu.formlibrary
         from uu.formlibrary.zope2 import initialize
+
+
+def test_request():
+    """ 
+    make request suitable for browser views and Zope2 security.
+    """
+    response = HTTPResponse(stdout=sys.stdout)
+    request = HTTPRequest(sys.stdin,
+                      {'SERVER_NAME'    : 'localhost',
+                       'SERVER_PORT'    : '80',
+                       'REQUEST_METHOD' : 'GET', },
+                      response)
+    setDefaultSkin(request)
+    alsoProvides(request, IFormLayer) #suitable for testing z3c.form views
+    return request
 

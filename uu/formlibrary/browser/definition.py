2912f73dac34880e84f37142c447ebfd64b3e892
Sean Upton <sdupton@gmail.com>
2011-11-22 12:41:12 -0700

    form view/load correctly loads data from form instance data-set/entry/record objects for all form groups, including default fieldset.

--- uu/formlibrary/browser/definition.py
+++ uu/formlibrary/browser/definition.py
@@ -1,8 +1,11 @@
+from collective.z3cform.datagridfield import DataGridField
 from plone.z3cform.interfaces import IWrappedForm
+from z3c.form.interfaces import IDataConverter
+from zope.component import getMultiAdapter
 from zope.interface import alsoProvides
 from zope.schema import getFieldNamesInOrder
 
-from uu.formlibrary.interfaces import IFormDefinition
+from uu.formlibrary.interfaces import IFormDefinition, ISimpleForm
 from uu.formlibrary.forms import ComposedForm
 
 
@@ -24,11 +27,52 @@ class FormView(object):
         return self._form.groups
    
     def render_form(self):
-        self._form.update()
+        for group in self._form.groups:
+            fieldgroup = self._form.components.groups[group.__name__]
+            if fieldgroup.group_usage != 'grid':
+                group.updateWidgets()
+        self._form.updateWidgets()
+        if self._form.save_attempt:
+            data, errors = self._form.extractData()
         return self._form.render()
+   
+    def _load_form_values_from_data(self):
+        # call only after calling self._form.update()a
+        if not ISimpleForm.providedBy(self.context):
+            return # don't update definition or non-data context
+        widget_prefix = 'form.widgets'
+        form = self._form 
+        data = self.context.data
+        req = self.request
+        groups = dict([(g.__name__, g) for g in form.groups])
+        groupnames = [''] + groups.keys()
+        for groupname in groupnames:
+            group_data = data.get(groupname, None)  #for default group
+            if groupname is '':
+                group = form
+            else:
+                group = groups[groupname]
+            if group_data is not None:
+                for name, formfield in group.fields.items():
+                    fullname = '.'.join((widget_prefix, name))
+                    v_req = req.get(fullname, None)
+                    if v_req is None:
+                        # field not found in request, get from data
+                        schema_name = name.replace('%s.' % groupname, '')
+                        v_data = getattr(group_data, schema_name, None)
+                        if v_data is not None:
+                            spec = (formfield.field, group.widgets[name])
+                            converter = getMultiAdapter(spec, IDataConverter)
+                            if not form.save_attempt and isinstance(
+                                    spec[1],
+                                    DataGridField):
+                                spec[1].value = v_data #sets, updates widgets
+                            req.set(fullname, converter.toWidgetValue(v_data))
     
     def update(self, *args, **kwargs):
         self._form.update(*args, **kwargs)
+        if self.definition is not self.context:
+            self._load_form_values_from_data()
     
     def __call__(self, *args, **kwargs):
         self.update(*args, **kwargs)

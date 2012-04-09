f17ccd56df11fe9b4974947a93566ec0aa780c4c
Sean Upton <sdupton@gmail.com>
2012-04-08 23:18:21 -0600

    initially working populate view

--- uu/formlibrary/browser/populate.py
+++ uu/formlibrary/browser/populate.py
@@ -1,24 +1,59 @@
-from plone.autoform import AutoExtensibleForm
+from itertools import product
+
+from plone.autoform.form import AutoExtensibleForm
+from plone.i18n.normalizer.interfaces import IURLNormalizer
+from z3c.form import button, form
+from zope.component import queryUtility
 from Products.statusmessages.interfaces import IStatusMessage
 
-from uu.formlibrary.interfaces imrpot IPopulateForms, IPeriodicSeries
+from uu.formlibrary.interfaces import IPopulateForms, IPeriodicSeries
+from uu.formlibrary.interfaces import FORM_TYPE_NAMES
+from uu.formlibrary import utils
 
+
+strip = lambda v: v.strip()
 
 def construct(context, type_name, id):
     context.invokeFactory(id=id, type_name=type_name)
     return context[id]
 
 
-def series_range(context, request):
+def series_range(context, formdata):
     """
     Returns series start, end, frequency label
     """
-    # TODO: support range overrides from request.
+    # TODO: support range overrides from formdata (IPeriodicSeries) if 
+    # override is specified.
     end = context.end or date(date.today().year,12,31) #default:EOY
-    return context.start, end, context.frequency
+    return context.start, end, context.frequency, context.active_weekdays
 
 
-class PopulateForms(AutoExtensibleForm):
+def title_variants(info, prefixes, suffixes):
+    """For one range info object, return id, title variants/permutations"""
+    result = []
+    prefixes = filter(lambda v: bool(v), map(strip, prefixes))
+    sufixes = filter(lambda v: bool(v), map(strip, suffixes))
+    normalizer = queryUtility(IURLNormalizer)
+    if prefixes and suffixes:
+        for prefix, suffix in product(prefixes, suffixes):
+            title = u'%s - %s - %s' % (prefix, info['title'], suffix)
+            id = normalizer.normalize(title)
+            result.append((id, title))
+    elif not prefixes and not suffixes:
+        title = info['title']
+        result.append(normalizer.normalize(title), title)
+    elif prefixes:
+        for prefix in prefixes:
+            title = u'%s - %s' % (prefix, info['title'])
+            result.append(normalizer.normalize(title), title)
+    elif suffixes:
+        for suffix in suffixes:
+            title = u'%s - %s' % (info['title'], suffix)
+            result.append(normalizer.normalize(title), title)
+    return result
+
+
+class PopulateForms(AutoExtensibleForm, form.Form):
     """Populate forms wizard for form series"""
     
     ## settins for auto-form:
@@ -27,23 +62,34 @@ class PopulateForms(AutoExtensibleForm):
     enable_form_tabbing = False  # display without form tabs
     
     schema = IPopulateForms
-    additionalSchemata = IPeriodicSeries
+    additionalSchemata = (IPeriodicSeries,)
 
     def __init__(self, context, request):
         super(PopulateForms, self).__init__(context, request)
-        self.status = IStatusMessage(self.request)
+        ## don't use self.status from z3c.form, use plone status
+        ## messages from Products.statusmessage
+        self._status = IStatusMessage(self.request)
     
     def _nothing_to_do(self, reason=None):
         msg = u'Cannot populate forms: missing information'
         if reason is not None:
             msg += u' (%s)' % reason
-        self.status.addStatusMessage(msg, type='info')
+        self._status.addStatusMessage(msg, type='info')
 
-    def update(self, *args, **kwargs):
-        super(PopulateForms, self).update(*args, **kwargs)
-        typename = str(self.request.form.get('form_type'))
-        ## TODO : get info for each period
-        start, end, freq, weekdays = series_range(self.context)
+    @button.buttonAndHandler(u'Create forms using these rules')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            import pdb; pdb.set_trace()
+            self._status.addStatusMessage(
+                u'There were errors populating forms.',
+                type='error',
+                )
+            return
+        created_count = 0
+        typename = str(data['form_type'])
+        typelabel = FORM_TYPE_NAMES.get(typename, unicode(typename))
+        start, end, freq, weekdays = series_range(self.context, data)
         if start is None:
             return self._nothing_to_do(u'No start date specified.')
         if freq == 'Weekly':
@@ -62,8 +108,27 @@ class PopulateForms(AutoExtensibleForm):
                 }[freq]  
         infos = [dict(infocls(d)) for d in infocls(start).all_until(end)]
         for info in infos:
-            pass # TODO : implement
-
-
-        ## TODO : call factory adapter.
+            ## every date slice
+            variants = title_variants(
+                info=info,
+                prefixes=data['title_prefixes'],
+                suffixes=data['title_suffixes'],
+                )
+            for id, title in variants:
+                ## every title/id permutation for each date slice
+                form = construct(self.context, typename, id)
+                form.title = title
+                form.start = info['start']
+                form.end = info['end']
+                form.definition = data.get('definition')
+                form.description = u''
+                form.reindexObject()
+                created_count += 1
+        self._status.addStatusMessage(
+            u'%s form items created of type %s' % (
+                created_count,
+                typelabel,
+                ),
+            type='info',
+            )
 

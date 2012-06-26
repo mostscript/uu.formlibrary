37f885d35f892f2ada56728334bce3728788d877
Sean Upton <sdupton@gmail.com>
2012-06-26 12:15:20 -0600

    filter/query definition interfaces (for content types)

--- uu/formlibrary/search/interfaces.py
+++ uu/formlibrary/search/interfaces.py
@@ -1,10 +1,31 @@
-from zope.interface import Interface
+from plone.directives import form
+from plone.formwidget.contenttree import ContentTreeFieldWidget
+from plone.formwidget.contenttree.source import UUIDSourceBinder
+from zope.interface import Interface, Invalid, invariant
 from zope.interface.common.mapping import IIterableMapping
 from zope.publisher.interfaces import IPublishTraverse
 from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 
 API_VERSION = 1
+COMPARATORS = (
+    'All',
+    'Any',
+    'Contains',
+    'DoesNotContain',
+    'Eq',
+    'Ge',
+    'Gt',
+    'InRange',
+    'Le',
+    'Lt',
+    'NotEq',
+    'NotInRange',
+    )
+COMPARATOR_VOCABULARY = SimpleVocabulary(
+    [SimpleTerm(k) for k in COMPARATORS]
+    )
 
 
 class ISearchAPICapability(IPublishTraverse):
@@ -113,4 +134,141 @@ class ISearchAPI(IIterableMapping, IPublishTraverse):
         with a human-readable listing of capability names 
         ('comparators', 'fields', etc).
         """
+
+
+class IFieldQuery(Interface):
+    """Represent query of a single field"""
+    
+    fieldname = schema.BytesLine(
+        title=u'Field name',
+        description=u'Name of field on schema for some context.',
+        required=True,
+        )
+    
+    comparator = schema.Choice(
+        title=u'Comparator',
+        vocabulary=COMPARATOR_VOCABULARY,
+        required=True,
+        )
+    
+    value = schema.Object(
+        title=u'Query value',
+        schema=Interface,
+        required=True,
+        )  # arbitrary, duck-typed value
+    
+    fieldtype = schema.InterfaceField(
+        title=u'Field type',
+        description=u'Field type interface',
+        required=True,
+        )  # zope.schema.interfaces.*
+    
+    value_type = schema.InterfaceField(
+        title=u'Value type',
+        description=u'Optional, only used for List/sequence fields.',
+        required=False,
+        )
+    
+    def validate(iface):
+        """
+        Given an interface, validate that the configured field
+        name / type / value_type are included in the interface.  May
+        be used for generating warnings to users about compatibility
+        of a query with an interface.
+        """
+
+
+class IBaseFilter(form.Schema):
+    """Base interface for filters"""
+    
+    title = schema.TextLine(
+        title=u'Title',
+        description=u'Title or name of filter.',
+        required=True,
+        )
+    
+    description = schema.Text(
+        title=u'Description',
+        required=False,
+        )
+    
+    def __call__(context):
+        """
+        Given a context of a form, return a matching result set for the
+        filter applied.
+        """
+
+
+class IRecordFilter(IBaseFilter):
+    """
+    A record filter is a named item that stores a set of field queries
+    for use on a search context.
+    """
+    
+    operator = schema.Choice(
+        title=u'Cross-field operator',
+        description=u'Choose AND (intersection) or OR (union) for combining '\
+                    u'results of each field query.',
+        vocabulary=SimpleVocabulary(map(lambda t: SimpleTerm(t), ('AND', 'OR'))),
+        default='AND',
+        )
+    
+    # query is not maintained in edit form of record filter, needs own 
+    # view for editing, because the three primary fields of IFieldQuery
+    # are sequentially defined (contingent) and a JavaScript implementation
+    # using the filters/comparators APIs is more sensible.
+    form.omitted('queries')
+    queries = schema.List(
+        title=u'Queries',
+        description=u'List of IFieldQuery objects, one or more per field.',
+        value_type=schema.Object(schema=IFieldQuery),
+        )
+    
+    def validate():
+        """
+        For the given schema context, validate each query in self.queries,
+        using the validate(iface) method of each query object and the 
+        interface/schema bound to the context of this filter.
+        """
+
+
+class ICompositeFilter(IBaseFilter):
+    """A composite filter """
+    
+    form.widget(filter_a=ContentTreeFieldWidget)
+    filter_a = schema.Choice(
+        title=u'Filter A',
+        description=u'The first filter to consider for set operations.',
+        source=UUIDSourceBinder(object_provides=IRecordFilter.__identifier__),
+        required=True,
+        )
+    
+    form.widget(filter_b=ContentTreeFieldWidget)
+    filter_b = schema.Choice(
+        title=u'Filter B',
+        description=u'The first filter to consider for set operations.',
+        source=UUIDSourceBinder(object_provides=IRecordFilter.__identifier__),
+        required=True,
+        )
+    
+    set_operator = schema.Choice(
+        title=u'Set operation',
+        description=u'The operation to perform to compute a given result '\
+                    u'set from the results of two specified filters.',
+        vocabulary=SimpleVocabulary(
+            map(lambda s:SimpleTerm(value=s[0], title=s[1]),
+                [('union', u'\u222a Union (OR)'),
+                 ('intersection', u'\u2229 Intersection (AND)'),
+                 ('difference', u'Relative complement (A\\B \u2261 A\u2212B)'),
+                ],
+                )
+            ),
+        default='union',
+        )
+    
+    @invariant
+    def distinct_filters(data):
+        if data.filter_a and data.filter_b:
+            if data.filter_a == data.filter_b:
+                raise Invalid('Filters A and B must be distinct, not same.')
 

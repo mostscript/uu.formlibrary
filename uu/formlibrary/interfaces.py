59adc00439c0f0196b333c3e718a7511fee416dd
Sean Upton <sdupton@gmail.com>
2013-04-22 09:48:45 -0600

    get ordered container interface from zope.container, not deprecated zope.app.container location

--- uu/formlibrary/interfaces.py
+++ uu/formlibrary/interfaces.py
@@ -9,7 +9,7 @@ from plone.formwidget.contenttree import ContentTreeFieldWidget
 from plone.formwidget.contenttree import MultiContentTreeFieldWidget
 from plone.uuid.interfaces import IUUID, IAttributeUUID
 from z3c.form.browser.textarea import TextAreaFieldWidget
-from zope.app.container.interfaces import IOrderedContainer
+from zope.container.interfaces import IOrderedContainer
 from zope.interface import Interface, Invalid, invariant
 from zope.interface.interfaces import IInterface
 from zope.interface.common.mapping import IMapping

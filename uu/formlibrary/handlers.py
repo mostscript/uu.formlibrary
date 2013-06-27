112e901a13dd102f480947e49b7cbb289ce395e5
Sean Upton <sdupton@gmail.com>
2013-06-27 11:49:48 -0600

    flake8 fixes, handlers.py

--- uu/formlibrary/handlers.py
+++ uu/formlibrary/handlers.py
@@ -29,7 +29,7 @@ def _ignorepaths(request):
     if key not in anno:
         anno[key] = []
     return anno.get(key)
-    
+
 
 def before_site_delete(context, event):
     request = getRequest()
@@ -39,7 +39,7 @@ def before_site_delete(context, event):
 
 
 def copyroles(source, dest):
-    """ 
+    """
     recursive copy of local roles for a copied content item or tree thereof.
     """
     local_roles = source.get_local_roles()
@@ -57,7 +57,7 @@ def definition_delete_integrity(context, event):
         path = context.getPhysicalPath()
         for base in _ignorepaths(request):
             if tuple(path[:len(base)]) == base:
-                # allow deletion of Plone site marked by before_site_delete() 
+                # allow deletion of Plone site marked by before_site_delete()
                 return
         formset = IFormSet(context)
         if len(formset) > 0:
@@ -67,7 +67,7 @@ def definition_delete_integrity(context, event):
 
 
 def handle_copypaste_local_roles(context, event):
-    """ 
+    """
     Event handler for subscriber to object, IObjectCopiedEvent that
     copies explicit (not computed from context) local roles from
     source to destination.
@@ -88,7 +88,7 @@ def sync_simple_form(form, components):
         if group.group_usage == u'grid':
             schema = grid_wrapper_schema(group.schema)
         if name not in form.data:
-            form.data[name] = FormEntry() # add new
+            form.data[name] = FormEntry()  # add new
         form.data[name].sign(schema)
     history_log(form, 'Schema updated for form entries.')
 
@@ -98,7 +98,7 @@ def sync_multi_form(form, definition):
     Update all contained form entry records, signed with definition
     schema.
     """
-    modified = False 
+    modified = False
     for entry in form.values():
         oldsig = entry.signature
         entry.sign(definition.schema)
@@ -115,7 +115,7 @@ def form_configuration_modified(context, event):
     try:
         definition = form_definition(context)
     except ValueError:
-        return # no definition means no schema to sync
+        return  # no definition means no schema to sync
     if ISimpleForm.providedBy(context):
         components = IFormComponents(definition)
         sync_simple_form(context, components)
@@ -129,9 +129,9 @@ def update_form_entries(context):
         definition = context.__parent__
     bound_forms = IFormSet(definition)
     simple_forms = [f for f in bound_forms.values()
-        if ISimpleForm.providedBy(f)] #TODO: named IFormSet adapter instead???
+                    if ISimpleForm.providedBy(f)]
     multi_forms = [f for f in bound_forms.values()
-        if IMultiForm.providedBy(f)]
+                   if IMultiForm.providedBy(f)]
     wftool = getToolByName(definition, 'portal_workflow')
     components = IFormComponents(definition)
     for form in simple_forms:
@@ -141,43 +141,44 @@ def update_form_entries(context):
         if wftool.getInfoFor(form, 'review_state') == 'visible':
             sync_multi_form(form, definition)
 
+
 def definition_schema_handler(context, event):
     """
-    Event handler for object modification parses XML schema definition with 
+    Event handler for object modification parses XML schema definition with
     plone.supermodel (via local ISchemaSaver utility), then saves:
-        
+
         * context.signature (persistent attribute)
-        
+
         * context._v_schema (convenient caching for current thread)**
-        
+
             **  other threads can load this on-demand, that is left to
                 the FormDefinition.schema property implementation.
-    
+
     This handler also normalizes the XML schema string in context.entry_schema
     with context.entry_schema.strip() (just in case it is needed).
-    
+
     Does nothing if entry_schema is absent/empty.
     """
     if context.entry_schema:
         ## serialization fixups and normalization:
         schema = context.entry_schema = context.entry_schema.strip()
-        schema = schema.replace('\r','') # remove all CRs
+        schema = schema.replace('\r', '')  # remove all CRs
         if (schema == DEFAULT_MODEL_XML and
                 context.signature == DEFAULT_SIGNATURE):
-            return # initial schema, not modification of schema; done.
+            return  # initial schema, not modification of schema; done.
         # normalize xml: reserialize as a check on manual edits:
         schema = serializeSchema(parse_schema(schema))
         if context.entry_schema != schema:
             context.entry_schema = schema           # save normalized xml
         saver = queryUtility(ISchemaSaver)          # get local utility
-        sig = context.signature = saver.add(schema) # persist xml/sig in saver
-        context._v_schema = (sig, saver.load(saver.get(sig))) # cache schema
+        sig = context.signature = saver.add(schema)  # persist xml/sig in saver
+        context._v_schema = (sig, saver.load(saver.get(sig)))  # cache schema
         update_form_entries(context)
 
 
 def reserialize(context, schema):
     if not IDefinitionBase.providedBy(context.__parent__):
-        return #not an event we care about
+        return  # not an event we care about
     definition = context.__parent__
     saver = queryUtility(ISchemaSaver)
     new_signature = saver.add(schema)
@@ -188,7 +189,7 @@ def reserialize(context, schema):
         update_form_entries(definition)
     # TODO: deal with nested groups and associated data objects contained
     if hasattr(aq_base(definition), '_v_schema'):
-        delattr(aq_base(definition), '_v_schema') #invalidate previous schema
+        delattr(aq_base(definition), '_v_schema')  # invalidate previous schema
     definition.reindexObject()
 
 

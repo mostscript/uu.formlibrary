0dfe19453de82ef6b24e83e96ac9f5aa9f1d2001
Sean Upton <sdupton@gmail.com>
2014-11-22 10:40:12 -0700

    code style cleanups in formcommon.js

--- uu/formlibrary/browser/resources/js/formcommon.js
+++ uu/formlibrary/browser/resources/js/formcommon.js
@@ -4,30 +4,42 @@
 
     "use strict";
 
-    $(document).ready(function(){
-        var viewname = $('#formcore').attr('data-viewname');
-        if (viewname == 'edit') {
-            /* hide portlet columns via deco.gs class removal */
-            $('#portal-column-content').removeClass('position-1:4');
-            $('#portal-column-content').removeClass('width-3:4');
-            $('#portal-column-two').removeClass('position-3:4');
-            $('#portal-column-two').removeClass('width-1:4');
-            /* change deco.gs classes for main column to full-width */
-            $('#portal-column-content').addClass('position-0');
-            $('#portal-column-content').addClass('width-full');
+    var msgShow = '[SHOW metadata and instructions.]',
+        msgHide = '[HIDE metadata and instructions.]';
+
+    function hookupMetaToggle() {
+        var viewname = $('#formcore').attr('data-viewname'),
+            colContent = $('#portal-column-content'),
+            colTwo = $('#portal-column-two'),
+            toggle = $('a.meta-toggle');
+        if (viewname === 'edit') {
+            /* hide portlet columns, main column to full width via deco.gs
+             * class names modification of defaults 
+             */
+            colContent
+                .removeClass('position-1:4 width-3:4')
+                .addClass('position-0 width-full');
+            colTwo.removeClass('position-3:4 width-1:4');
+        }
+        toggle.click(
+            function () {
+                var link = $(this),
+                    hidden = link.text() === msgHide,
+                    wrapper = link.parents('.form-meta').children('.wrapper');
+                link.text((hidden) ? msgShow : msgHide);
+                wrapper.slideToggle();
             }
-        $('a.meta-toggle').click(function() {
-            var clicklink = $(this);
-            if (clicklink.text() == '[HIDE metadata and instructions.]') {
-                clicklink.text('[SHOW metadata and instructions.]');
-            }
-            else {
-                clicklink.text('[HIDE metadata and instructions.]');
-            }
-            clicklink.parents('div.form-meta').children('div.wrapper').slideToggle();
-            });
-        // finally remove any z3cformInlineValidation classes from ANY elements
+        );
+    }
+
+    function disableInlineValidation() {
+        // remove all z3cformInlineValidation classes everywhere
         $('.z3cformInlineValidation').removeClass('z3cformInlineValidation');
+    }
+
+    $(document).ready(function(){
+        hookupMetaToggle();
+        disableInlineValidation();
     });
 
 }(jQuery));

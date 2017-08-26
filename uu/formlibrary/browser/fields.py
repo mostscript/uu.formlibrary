da79ab6f5c4b0078433b701aee508906c859f7dc
Sean Upton <sdupton@gmail.com>
2017-08-26 17:03:04 -0600

    Schema editor listing uses common widget updates, just like preview and instances of forms being edited

--- uu/formlibrary/browser/fields.py
+++ uu/formlibrary/browser/fields.py
@@ -3,17 +3,22 @@ from plone.z3cform.layout import FormWrapper
 from Products.CMFCore.utils import getToolByName
 from zope.component.hooks import getSite
 
+from uu.formlibrary.forms import common_widget_updates
 from definition import DefinitionCommon
 
 
 class ContextSchemaListing(SchemaListing):
-    
+
     ignoreContext = True
 
     @property
     def additionalSchemata(self):
         return ()
-    
+
+    def updateWidgets(self):
+        common_widget_updates(self)
+        super(ContextSchemaListing, self).updateWidgets()
+
     def __init__(self, context, request):
         super(ContextSchemaListing, self).__init__(context, request)
 
